fa5f15557479d9c16d47840d0db6118fe13fe894
@rgalicia <rigo.galicia@cooperativa-tonantel.com.gt>
2018-12-10 16:47:28 -0600

    Modificacion de Imagenes

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,12 @@
       b-navbar-toggle(target='nav_collapse')
       b-collapse#nav_collapse(is-nav='')
         b-navbar-nav
-          b-nav-item(href='#') INICIO
+          b-nav-item(href='#card-form') INICIO
           b-nav-item(href='#') SERVICIOS
           b-nav-item(href='#') CONTACTANOS
-    div
-      b-carousel#carousel1(style='text-shadow: 1px 1px 2px #333;', controls='', indicators='', background='#ababab', :interval='4000', img-width='1024', img-height='480', v-model='slide', @sliding-start='onSlideStart', @sliding-end='onSlideEnd')
-        b-carousel-slide(img-src='https://raw.githubusercontent.com/rigogalicia/Landing_Supertienda/master/src/assets/Logo-fondo.jpeg')
-        b-carousel-slide(caption='First slide', text='Nulla vitae elit libero, a pharetra augue mollis interdum.', img-src='https://picsum.photos/1024/480/?image=52')
-        b-carousel-slide(caption='First slide', text='Nulla vitae elit libero, a pharetra augue mollis interdum.', img-src='https://picsum.photos/1024/480/?image=52')
-    img(src="@/assets/prueba.png")
+
+    comp-carousel
+
     .margen-content.row
       .col-md-3
       .col-md-6
--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -1,14 +1,7 @@
 <template lang="pug">
   div
     b-carousel#carousel1(style='text-shadow: 1px 1px 2px #333;', controls='', indicators='', background='#ababab', :interval='4000', img-width='1024', img-height='480', v-model='slide', @sliding-start='onSlideStart', @sliding-end='onSlideEnd')
-        b-carousel-slide(caption='First slide', text='Nulla vitae elit libero, a pharetra augue mollis interdum.', :img-src='this.listImg.img_1')
-        b-carousel-slide(caption='First slide', text='Nulla vitae elit libero, a pharetra augue mollis interdum.', img-src='https://picsum.photos/1024/480/?image=52')
-        b-carousel-slide(caption='First slide', text='Nulla vitae elit libero, a pharetra augue mollis interdum.', img-src='https://picsum.photos/1024/480/?image=52')
+        b-carousel-slide(img-src='https://raw.githubusercontent.com/rigogalicia/Landing_Supertienda/master/src/assets/Logo-fondo.jpeg')
+        b-carousel-slide(img-src='https://raw.githubusercontent.com/rigogalicia/Landing_Supertienda/master/src/assets/Bazar.jpeg')
+        b-carousel-slide(img-src='https://raw.githubusercontent.com/rigogalicia/Landing_Supertienda/master/src/assets/Canastas.jpeg')
 </template>
-<script>
-export default {
-  props: {
-    listImg: {type: Object}
-  }
-}
-</script>
