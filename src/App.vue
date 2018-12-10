6e18366d0d9e4afccb510a3e759cd45da3bff9e8
@rgalicia <rigo.galicia@cooperativa-tonantel.com.gt>
2018-12-10 13:15:23 -0600

    Version con formulario y card

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,36 @@
           b-nav-item(href='#') INICIO
           b-nav-item(href='#') SERVICIOS
           b-nav-item(href='#') CONTACTANOS
+
     comp-carousel
+    
+    .margen-content.row
+      .col-md-3
+      .col-md-6
+        #card-form.card
+          .card-body
+            comp-form-contacto
+      .col-md-3
+    
+    .margen-content.row
+      .col-md-4
+        comp-card
+      .col-md-4
+        comp-card
+      .col-md-4
+        comp-card
+    
 </template>
 
 <script>
 import CompCarousel from '@/components/carousel.vue'
+import CompFormContacto from '@/components/formulario.vue'
+import CompCard from '@/components/card.vue'
 
 export default {
   name: 'app',
   components: {
-    CompCarousel
+    CompCarousel, CompFormContacto, CompCard
   },
   data () {
     return {
---
+++ src/components/formulario.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  div
+    b-form(@submit='onSubmit', @reset='onReset')
+      b-form-group(label='Nombre:', label-for='nombre')
+        b-form-input#nombre(type='text', required='true', placeholder='Ingresa tu nombre')
+      b-form-group(label='Email:', label-for='email')
+        b-form-input#email(type='email', placeholder='Ingresa tu correo electrónico')
+      b-form-group(label='Teléfono:', label-for='telefono')
+        b-form-input#telefono(type='text', placeholder='Ingresa número de Teléfono')
+      b-form-group(label='Comentario:', label-for='comentario')
+        b-form-textarea#comentario(placeholder='Ingresa un comentario', :rows='3', :max-rows='6')
+      b-button(type='submit', variant='primary') Registrarse
+</template>
