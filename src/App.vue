af851b3d761a4ea2414052358409ac9cc9293c86
@rgalicia <rigo.galicia@cooperativa-tonantel.com.gt>
2018-12-11 14:30:43 -0600

    Nuevas imagenes

--- src/App.vue
+++ src/App.vue
@@ -9,23 +9,40 @@
           b-nav-item(href='#') CONTACTANOS
 
     comp-carousel
-
+    div
+      h1
+        strong Comentarios o Dudas
+      p Sera un gusto atenderle.
     .margen-content.row
       .col-md-3
       .col-md-6
         #card-form.card
           .card-body
+            
             comp-form-contacto
+            
       .col-md-3
-    
+        
+    .margen-content.row
+           
+      .col-md-4
+        
+        comp-card(imgUrl="https://picsum.photos/600/300/?image=25", titulo="Parqueo", descripcion="Disfruta de nuestro seguro y amplio parqueo, ubicadi a un costado de Pollo Pinulitos.")
+      .col-md-4
+       div
+        h1
+          strong Servicios
+        p Disfruta de nuestros multiples servicios.     
+        comp-card(imgUrl="https://raw.githubusercontent.com/rigogalicia/Landing_Supertienda/master/src/assets/WhatsApp%20Image%202018-12-01%20at%2013.29.56.jpeg", titulo="Electrodomesticos" descripcion="Las mejores marcas de amueblados, televisores, refrigeradires y mucho mas al mejor precio.")
+      .col-md-4
+        comp-card(imgUrl="https://picsum.photos/600/300/?image=25", titulo="Surtitienda" descripcion="Credito en productos para iniciar su negocio o surtirlo con mayor comodidad.")
     .margen-content.row
       .col-md-4
-        comp-card
+        comp-card(imgUrl="https://picsum.photos/600/300/?image=25", titulo="Agente Mi Coope", descripcion="Realiza tus pagos, depositos, retiros de forma facil, rapida y confiable.")
       .col-md-4
-        comp-card2
+        comp-card(imgUrl="https://picsum.photos/600/300/?image=25", titulo="Reparto a domicilio" descripcion="Llame al Tel: 5494 0620, con gusto tomaremos su pedido, aplican restricciones.")
       .col-md-4
-        comp-card
-    
+        comp-card(imgUrl="https://picsum.photos/600/300/?image=25", titulo="Productos de consumo diario" descripcion="Diversos productos que engloban las marcas mas conocidas al mejor precio.")
 </template>
 
 <script>
--- src/components/card.vue
+++ src/components/card.vue
@@ -1,6 +1,15 @@
 <template lang="pug">
   div
-    b-card.mb-2(title='Agente Mi Coope', img-src='https://picsum.photos/600/300/?image=25', img-alt='Image', img-top='', tag='article', style='max-width: 50rem;')
-        p.card-text Some quick example text to build on the card title and make up the bulk of the card's content.
-        b-button(href='#', variant='primary') Go somewhere
+    b-card.mb-2(:title='titulo', :img-src='imgUrl', img-alt='Image', img-top='', tag='article', style='max-width: 50rem;')
+        p.card-text {{ descripcion }}
+        b-button(href='#', variant='primary') Boton
 </template>
+<script>
+export default {
+  props: {
+    imgUrl: String,
+    titulo: String,
+    descripcion: String
+  }
+}
+</script>
