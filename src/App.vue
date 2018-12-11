b02003556279af467df09e34fbe90f9d88e02f7b
@rgalicia <rigo.galicia@cooperativa-tonantel.com.gt>
2018-12-11 17:34:34 -0600

    Nuevas imagenes

--- src/App.vue
+++ src/App.vue
@@ -4,45 +4,51 @@
       b-navbar-toggle(target='nav_collapse')
       b-collapse#nav_collapse(is-nav='')
         b-navbar-nav
-          b-nav-item(href='#card-form') INICIO
+          b-nav-item(href='#form-registro') INICIO
           b-nav-item(href='#') SERVICIOS
           b-nav-item(href='#') CONTACTANOS
 
     comp-carousel
-    div
+    div#form-registro
       h1
         strong Comentarios o Dudas
-      p Sera un gusto atenderle.
+      p.parrafo-titulo Sera un gusto atenderle.
     .margen-content.row
       .col-md-3
       .col-md-6
         #card-form.card
           .card-body
-            
-            comp-form-contacto
-            
+            comp-form-contacto   
       .col-md-3
-        
+    
     .margen-content.row
-           
-      .col-md-4
-        
-        comp-card(imgUrl="https://picsum.photos/600/300/?image=25", titulo="Parqueo", descripcion="Disfruta de nuestro seguro y amplio parqueo, ubicadi a un costado de Pollo Pinulitos.")
-      .col-md-4
-       div
+      .col-md-4 
+      .col-md-4 
         h1
           strong Servicios
-        p Disfruta de nuestros multiples servicios.     
-        comp-card(imgUrl="https://raw.githubusercontent.com/rigogalicia/Landing_Supertienda/master/src/assets/WhatsApp%20Image%202018-12-01%20at%2013.29.56.jpeg", titulo="Electrodomesticos" descripcion="Las mejores marcas de amueblados, televisores, refrigeradires y mucho mas al mejor precio.")
-      .col-md-4
-        comp-card(imgUrl="https://picsum.photos/600/300/?image=25", titulo="Surtitienda" descripcion="Credito en productos para iniciar su negocio o surtirlo con mayor comodidad.")
+        p.parrafo-titulo Disfruta de nuestra gran variedad de servicios.
+      .col-md-4 
+
     .margen-content.row
       .col-md-4
-        comp-card(imgUrl="https://picsum.photos/600/300/?image=25", titulo="Agente Mi Coope", descripcion="Realiza tus pagos, depositos, retiros de forma facil, rapida y confiable.")
+        comp-card(imgUrl="src/assets/autocar.jpg", titulo="Parqueo", descripcion="Disfruta de nuestro seguro y amplio parqueo, ubicadi a un costado de Pollo Pinulitos.")
+      .col-md-4    
+        comp-card(imgUrl="src/assets/electrocar.jpg", titulo="Electrodomesticos" descripcion="Las mejores marcas de amueblados, televisores, refrigeradires y mucho mas al mejor precio.")
       .col-md-4
-        comp-card(imgUrl="https://picsum.photos/600/300/?image=25", titulo="Reparto a domicilio" descripcion="Llame al Tel: 5494 0620, con gusto tomaremos su pedido, aplican restricciones.")
+        comp-card(imgUrl="src/assets/carsurtitienda.jpg", titulo="Surtitienda" descripcion="Credito en productos para iniciar su negocio o surtirlo con mayor comodidad.")
+    .margen-content.row
       .col-md-4
-        comp-card(imgUrl="https://picsum.photos/600/300/?image=25", titulo="Productos de consumo diario" descripcion="Diversos productos que engloban las marcas mas conocidas al mejor precio.")
+        comp-card(imgUrl="src/assets/agentecar.png", titulo="Agente Mi Coope", descripcion="Realiza tus pagos, depositos, retiros de forma facil, rapida y confiable.")
+      .col-md-4
+        comp-card(imgUrl="src/assets/repartocar.jpg", titulo="Reparto a domicilio" descripcion="Llame al Tel: 5494 0620, con gusto tomaremos su pedido, aplican restricciones.")
+      .col-md-4
+        comp-card(imgUrl="src/assets/carproducto.jpg", titulo="Productos de consumo diario" descripcion="Diversos productos que engloban las marcas mas conocidas al mejor precio.")
+
+    .margen-content.row
+    
+      a.fa.fa-facebook(href='#')
+
+    .margen-content.row
 </template>
 
 <script>
--- src/components/card.vue
+++ src/components/card.vue
@@ -2,7 +2,7 @@
   div
     b-card.mb-2(:title='titulo', :img-src='imgUrl', img-alt='Image', img-top='', tag='article', style='max-width: 50rem;')
         p.card-text {{ descripcion }}
-        b-button(href='#', variant='primary') Boton
+        
 </template>
 <script>
 export default {
